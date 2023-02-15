7a75a8c66e5d7ed247ef2f1a8327e286266ec446
pradhokot <99374274+pradhokot@users.noreply.github.com>
2023-02-15 11:12:30 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# waiters
+ template web digital menu supresso gallery & cafe menggunakan bootstrap, html, css, jquery
---
+++ head.js
@@ -0,0 +1,11 @@
+document.write(`
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="icon" href="assets/img/logo.ico">
+<link rel="stylesheet" href="assets/bootstrap-4.6.2-dist/css/bootstrap.min.css">
+<link rel="stylesheet" href="assets/bootstrap-icons-1.9.1/bootstrap-icons.css">
+<link rel="stylesheet" href="assets/fonts/gotham.css">
+<link rel="stylesheet" href="assets/css/custom.css">
+<link rel="stylesheet" href="assets/css/styles.css">
+`)
